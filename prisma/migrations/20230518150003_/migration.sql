6880e70f9d846c4fa06e1295655265dee2af40e5
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-05-18 23:07:01 +0630

    one to many

---
+++ prisma/migrations/20230518150003_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `assignedAt` on the `CategoriesOnPosts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CategoriesOnPosts" DROP COLUMN "assignedAt";
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { prisma } from "./client";
 
 const main = async () => {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  const postsByCat = await prisma.post.findMany({
+    where: { categories: { some: { name: "animal" } } },
+  });
+
+  console.log(postsByCat);
 };
 
 main();
